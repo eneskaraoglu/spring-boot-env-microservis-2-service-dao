10d47675b1a2d208dc8cf96976fd4c7a857075cb
enes k <eneskaraoglu@gmail.com>
2023-05-06 23:05:41 +0300

    init commit
    init commit

---
+++ src/main/java/com/eneskaraoglu/ek/Micro2ServiceDAO.java
@@ -0,0 +1,13 @@
+package com.eneskaraoglu.ek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro2ServiceDAO {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro2ServiceDAO.class, args);
+	}
+
+}
