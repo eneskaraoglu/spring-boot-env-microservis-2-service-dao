06456c0a2386bf9f8ccff532273e973ede45626d
enes k <eneskaraoglu@gmail.com>
2023-05-09 23:48:05 +0300

    proxy ile depo ve katalog dervisleri bağlandı
    proxy ile depo ve katalog dervisleri bağlandı

--- src/main/java/com/eneskaraoglu/ek/lib/M1DataRestProxy.java
+++ src/main/java/com/eneskaraoglu/ek/lib/M1DataRestProxy.java
@@ -1,17 +1,50 @@
 package com.eneskaraoglu.ek.lib;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.eneskaraoglu.ek.entity.Depo;
+import com.eneskaraoglu.ek.entity.Katalog;
 
-@NoRepositoryBean
 @FeignClient(name="spring-boot-env-microservis-1-data-rest", url="localhost:8080")
 public interface M1DataRestProxy {
 
-	@GetMapping("/depo/{id}")
+	@GetMapping("data-rest/depoes/{id}")
 	public Depo getDepo(@PathVariable int id);
 	
+	@GetMapping("data-rest/depoes")
+	public List<Depo> getDepo();
+	
+	@PostMapping("data-rest/depoes")
+	public Depo addDepo(@RequestBody Depo theEntity);
+	
+	@DeleteMapping("data-rest/depoes/{id}")
+	public String deleteDepo(@PathVariable int id);
+	
+	@PutMapping("data-rest/depoes")
+	public Depo updateDepo(@RequestBody Depo theEntity);
+	
+
+
+	@GetMapping("data-rest/katalogs/{id}")
+	public Katalog getKatalog(@PathVariable int id);
+	
+	@GetMapping("data-rest/katalogs")
+	public List<Katalog> getKatalog();
+	
+	@PostMapping("data-rest/katalogs")
+	public Katalog addKatalog(@RequestBody Depo theEntity);
+	
+	@DeleteMapping("data-rest/katalogs/{id}")
+	public String deleteKatalog(@PathVariable int id);
+	
+	@PutMapping("data-rest/katalogs")
+	public Katalog updateKatalog(@RequestBody Depo theEntity);
 }
--- src/main/java/com/eneskaraoglu/ek/rest/MicroServiceController.java
+++ src/main/java/com/eneskaraoglu/ek/rest/MicroServiceController.java
@@ -1,6 +1,8 @@
 package com.eneskaraoglu.ek.rest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,38 +10,30 @@ import org.hibernate.mapping.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.eneskaraoglu.ek.entity.Depo;
+import com.eneskaraoglu.ek.entity.Envanter;
+import com.eneskaraoglu.ek.entity.Katalog;
 import com.eneskaraoglu.ek.lib.M1DataRestProxy;
 
 @RestController
 @RequestMapping("/env")
 public class MicroServiceController {
-	
-	
-	/*
-	@GetMapping("/depo/{id}")
-	public Depo getDepo(@PathVariable int id) {
-		
-
-		HashMap<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", String.valueOf(id));
-		
-		ResponseEntity<Depo> response = null;
-		RestTemplate restTemplate = new RestTemplate();
-		
-		String resourceUrl = "http://localhost:8080/data-rest/depoes/{id}";
-		response = restTemplate.getForEntity(resourceUrl + "",  Depo.class , uriVariable);
-
-		return response.getBody();
-	}*/
 	
 	@Autowired
 	private M1DataRestProxy proxy;
@@ -47,35 +41,106 @@ public class MicroServiceController {
 	public MicroServiceController(M1DataRestProxy theProxy) {
 		theProxy = proxy;
 	};
+
+	//Burada restTemplate kullanıldı
+	@GetMapping("/depoRT/{id}")
+	public Depo getDepoRT(@PathVariable int id) {
+		HashMap<String, String> uriVariable = new HashMap<>();
+		uriVariable.put("id", String.valueOf(id));
+		String resourceUrl = "http://localhost:8080/data-rest/depoes/{id}";
+		ResponseEntity<Depo> response = new RestTemplate().getForEntity(resourceUrl + "",  Depo.class , uriVariable);
+		return response.getBody();
+	}
 	
-	//Burada resttemplete yerine proxy kullandım.
+	@GetMapping("/depoRT")
+	public List<Depo> getDepoRT() {
+		System.out.println("---------1------------------");
+		String resourceUrl = "http://localhost:8080/data-rest/depoes";
+		List<Depo> response = (List<Depo>) restTemplate().getForEntity(resourceUrl + "",  List.class );
+		System.out.println("------------3---------------"+response);
+		return response;
+	}
+	
+
+	@Bean
+	public RestTemplate restTemplate() {
+	   final RestTemplate restTemplate = new RestTemplate();
+
+	   List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+	   MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+	   converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+	   messageConverters.add(converter);
+	   restTemplate.setMessageConverters(messageConverters);
+
+	   return restTemplate;
+	}
+	
+	//Burada resttemplete yerine proxy kullanıldı
 	//bunun için
 	//Micro2ServiceDAO da @EnableFeignClients 
 	//pom.xlm <artifactId>spring-cloud-starter-openfeign</artifactId>
 	//M1DataRestProxy @FeignClient(name="spring-boot-env-microservis-1-data-rest", url="localhost:8080")
 	@GetMapping("/depo/{id}")
 	public Depo getDepo(@PathVariable int id) {
-		
-		HashMap<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", String.valueOf(id));
-		Depo response = null;
-		RestTemplate restTemplate = new RestTemplate();
-		String resourceUrl = "http://localhost:8080/data-rest/depoes/{id}";
-		response = proxy.getDepo(id);
-
+		Depo response = proxy.getDepo(id);
 		return response;
-	}	
+	}
 	
 	@GetMapping("/depo")
 	public List<Depo> getDepo() {
-		System.out.println("---------1------------------");
-		RestTemplate restTemplate = new RestTemplate();
-		System.out.println("----------2-----------------");
-		String resourceUrl = "http://localhost:8080/data-rest/depoes";
-		ResponseEntity<Depo[]> response = restTemplate.exchange(resourceUrl + "", HttpMethod.GET, null , Depo[].class );
-		System.out.println("------------3---------------");
-		System.out.println(response.getBody());
-		return Arrays.asList(response.getBody());
+		System.out.println("proxy.getDepo:"+proxy.getDepo());
+		List<Depo> response = proxy.getDepo();
+		return response;
 	}
 	
+	@PostMapping("/depo")
+	public Depo addDepo(@RequestBody Depo theEntity) {
+		Depo response = proxy.addDepo(theEntity);
+		return response;
+	}
+
+	@DeleteMapping("/depo/{id}")
+	public String deleteDepo(@PathVariable int id) {
+		String response = proxy.deleteDepo(id);
+		return response;
+	}
+	
+	@PutMapping("/depo")
+	public Depo updateDepo(@RequestBody Depo theEntity) {
+		Depo response = proxy.updateDepo(theEntity);
+		return response;
+	}
+	
+	//////
+	
+	@GetMapping("/katalog/{id}")
+	public Katalog getKatalog(@PathVariable int id) {
+		Katalog response = proxy.getKatalog(id);
+		return response;
+	}
+	
+	@GetMapping("/katalog")
+	public List<Katalog> getKatalog() {
+		System.out.println("proxy.getDepo:"+proxy.getDepo());
+		List<Katalog> response = proxy.getKatalog();
+		return response;
+	}
+	
+	@PostMapping("/katalog")
+	public Katalog addKatalog(@RequestBody Depo theEntity) {
+		Katalog response = proxy.addKatalog(theEntity);
+		return response;
+	}
+
+	@DeleteMapping("/katalog/{id}")
+	public String deleteKatalog(@PathVariable int id) {
+		String response = proxy.deleteKatalog(id);
+		return response;
+	}
+	
+	@PutMapping("/katalog")
+	public Katalog updateKatalog(@RequestBody Depo theEntity) {
+		Katalog response = proxy.updateKatalog(theEntity);
+		return response;
+	}
 }
